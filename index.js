0f4b79c89eb09974964d314f622bffb9c98f08f7
rafi993 <fair7993@gmail.com>
2018-02-23 08:34:08 +0530

    git ignore files ignored and shame displayed

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
 const fs = require('fs');
-const walk = require('walk');
+const readline = require('readline');
 const commandLineArgs = require('command-line-args')
+const ignore = require('ignore')
+const chalk = require('chalk');
 
 // get command line argument if no shame factor given use 5 as default shame factor
-const optionDefinitions = [
-  { name:'shameThreshold', alias:'t', type:Number, defaultOption: true ,defaultValue : 5 }];
+const optionDefinitions = [{
+    name: 'shameThreshold',
+    alias: 't',
+    type: Number,
+    defaultOption: true,
+    defaultValue: 5
+}];
 const options = commandLineArgs(optionDefinitions)
-const shameThreshold = options.shameThreshold;
 
-console.log('Your shame threshold is', shameThreshold)
+// global variable declarations
+let ig;
+let files = {};
+let path = process.cwd()
+let ignoreLine = [];
+let shameList = [];
 
-// Read list of files in .gitignore
+/**
+ * Returns an array of shame from file
+ * @param {String} file File name from which to get list of shame
+ */
+const getShame = file => 
+    fs.readFileSync(file, "utf-8")
+        .match(/@shame(.*)/g);
 
+// @shame sdfsdfsdf
+/**
+ * Traverse all the folders in present dir and get their list 
+ * @param {String} path Path to navigate
+ * @param {Array} files Array to which naviated file names will be pushed
+ */
+const getFiles = (path, files) => {
+    fs.readdirSync(path).forEach(file => {
+        let subpath = path + '/' + file;
+        if (!ig.ignores(subpath))
+            if (fs.lstatSync(subpath).isDirectory()) {
+                getFiles(subpath, files);
+            } else {
+                let fileContent = getShame(path + '/' + file);
+                if(fileContent!==null)
+                    shameList[file] = fileContent;
+            }
+    });
+}
 
-let files   = [];
-
-// Traverse all the folders in present dir and get their list 
-const walker  = walk.walk('./', { followLinks: false });
-walker.on('file', (root, stat, next)=> {
-    // Add this file to the list of files
-    files.push(root + '/' + stat.name);
-    next();
-});
-
-walker.on('end', function() {
-    // console.log(files);
+// Navigate all files and folders except ones in .gitignore
+readline.createInterface({
+    input: fs.createReadStream('.gitignore'),
+    terminal: false
+}).on('line', line => {
+    ignoreLine.push(line)
+}).on('close', () => {
+    ig = ignore().add([...ignoreLine, '.git']);
+    getFiles(__dirname, files)
+    const flatShameList = [].concat.apply([],  Object.keys(shameList)
+                            .map(x=>shameList[x]));
+    if(flatShameList.length>options.shameThreshold){
+        console.log(chalk.bgRed.bold('You have reached shame threshold please reduce please reduce it'));
+        Object.keys(shameList).forEach(x=>{
+            console.log(chalk.bgBlue(x) +':')
+            shameList[x].map(y=>{
+                console.log('       '+y)
+            })
+        })
+    } else {
+        console.log(chalk.bgGreen(' You are good to go  '))
+    }
 });
